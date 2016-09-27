74b9f5e2a2e564608600b3c89803cc93110c8b37
Isotop7 <hendrik.biz@gmail.com>
2016-09-27 16:48:15 +0200

    add action handlers

--- Pyrtemonnaie.py
+++ Pyrtemonnaie.py
@@ -15,9 +15,17 @@ class Pyrtemonnaie(QtWidgets.QMainWindow):
         super(Pyrtemonnaie, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        
+
+        self.ui.actionPyrtemonnaie_ffnen.triggered.connect(self.triggerOpenPyrtemonnaie)
+        self.ui.actionPyrtemonnaie_speichern.triggered.connect(self.triggerSavePyrtemonnaie)
+        self.ui.actionKonfiguration_anzeigen.triggered.connect(self.triggerShowConfig)
+        self.ui.actionDatenpunkt_hinzuf_gen.triggered.connect(self.triggerAddDatapoint)
+        self.ui.actionDatenpunkt_bearbeiten.triggered.connect(self.triggerEditDatapoint)
+        self.ui.actionDatenpunkt_l_schen.triggered.connect(self.triggerDeleteDatapoint)
+        self.ui.actionPyrtemonnaie_anzeigen.triggered.connect(self.triggerShowPyrtemonnaie)
         self.ui.actionBeenden.triggered.connect(self.triggerBeenden)
     
+
         self.show()
 
     def triggerOpenPyrtemonnaie(self):
@@ -38,10 +46,9 @@ class Pyrtemonnaie(QtWidgets.QMainWindow):
     def triggerDeleteDatapoint(self):
         pass
 
-    def triggerShowDatapoint(self):
+    def triggerShowPyrtemonnaie(self):
         pass
     
-
     def triggerClose(self):
         self.close()
 
