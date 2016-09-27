433992cf886ad2c19e44a8d2abb4a7da3a70d9e8
Isotop7 <hendrik.biz@gmail.com>
2016-09-27 17:49:29 +0200

    add handler for opening files

--- Pyrtemonnaie.py
+++ Pyrtemonnaie.py
@@ -4,17 +4,20 @@ from collections import namedtuple
 from datetime import date
 from operator import attrgetter
 
-from PyQt5 import QtCore, QtGui, QtWidgets
-
+from PyQt5.QtWidgets import *
 from ui.Pyrtemonnaie_MainWindow_ui import Ui_MainWindow
 
 Datapoint = namedtuple('Datapoint', ['Recipient', 'Date', 'Value', 'Comment'])
 
-class Pyrtemonnaie(QtWidgets.QMainWindow):
+class Pyrtemonnaie(QMainWindow):
     def __init__(self):
         super(Pyrtemonnaie, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
+        self.Pyrtemonnaie = []
+        self.file_path = "database.dat"
+        self.file_loaded = False
 
         self.ui.actionOpenPyrtemonnaie.triggered.connect(self.triggerOpenPyrtemonnaie)
         self.ui.actionSavePyrtemonnaie.triggered.connect(self.triggerSavePyrtemonnaie)
@@ -29,8 +32,69 @@ class Pyrtemonnaie(QtWidgets.QMainWindow):
     
         self.show()
 
+    def parse_line(self, s):
+        def parse_line_date(s_date):
+            try:
+                if self.date_matches_regex(s_date):
+                    s_date = s_date.split(".")
+                    return date(int(s_date[2]), int(s_date[1]), int(s_date[0]))
+                else:
+                    raise ValueError
+            except ValueError and TypeError:
+                return date.today()
+
+        def parse_line_value(s_value):
+            if float(s_value) < 0:
+                return 0.0
+            else:
+                return float(s_value)
+
+        def parse_line_comment(s_comment):
+            if ";" in s_comment:
+                return ""
+            else:
+                return s_comment.rstrip()
+
+        try:
+            s = s.split(";")
+            if len(s) == 4:
+                return Datapoint(s[0], parse_line_date(s[1]), parse_line_value(s[2]), parse_line_comment(s[3]))
+            else:
+                raise IndexError
+        except IndexError:
+            return Datapoint("", date.today(), 0.0, "")
+
+    def date_matches_regex(self, s):
+        regex = re.compile(r'\d{2}\.\d{2}\.(\d{4}|\d{2})')
+        return regex.match(s)
+
     def triggerOpenPyrtemonnaie(self):
-        pass
+
+        def _activate_menu():
+            self.ui.menuPyrtemonnaie.setEnabled(True)
+            self.file_loaded = True
+
+        if len(self.Pyrtemonnaie) > 0:
+            confirm = QMessageBox.warning(self, "Pyrtemonnaie", "Es sind bereits Daten geladen. Wenn Sie fortfahren werden Ihre Änderungen eventuell überschrieben!", QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+            if confirm == QMessageBox.Yes:
+                self.file_loaded = False
+            else:
+                return            
+
+        try:
+            self.file_path = QFileDialog.getOpenFileName(self, "Open Pyrtemonnaie",  self.file_path)[0]
+            file_object = open(self.file_path, "r")
+            for line in file_object:
+                if line.isspace() == False:
+                    d = self.parse_line(line)
+                    self.Pyrtemonnaie.append(d)
+            file_object.close()
+            _activate_menu()
+            #self.dump_pyrtemonnaie_handler()
+        except ValueError:
+            QMessageBox.critical("Pyrtemonnaie", "Fehler beim Laden der Datei! Bitte überprüfen Sie den Dateiinhalt.", QMessageBox.Ok)
+        except IndexError:
+            QMessageBox.critical("Pyrtemonnaie", "Fehler beim Laden der Datei!\n'{line}\n' ist fehlerhaft.".format(line=line.rstrip()), QMessageBox.Ok)
 
     def triggerSavePyrtemonnaie(self):
         pass
